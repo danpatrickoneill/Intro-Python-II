43319e7e9e248eacc7b7041924a79823d0c0cb1b
danpatrickoneill <dan47573@gmail.com>
2019-10-02 18:56:46 -0400

    Added Room class w/ name, description and paths

--- src/room.py
+++ src/room.py
@@ -1,2 +1,28 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.paths = {}
+
+    def getName(self):
+        return self.name
+
+    def getDesc(self):
+        return self.description
+
+    def getPaths(self):
+        if hasattr(self, 'n_to'):
+            self.paths['n'] = self.n_to
+
+        if hasattr(self, 'e_to'):
+            self.paths['e'] = self.e_to
+
+        if hasattr(self, 's_to'):
+            self.paths['s'] = self.s_to
+
+        if hasattr(self, 'w_to'):
+            self.paths['w'] = self.w_to

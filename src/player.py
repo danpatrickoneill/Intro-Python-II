6a39b20d96012de09e03350981ffec56e1160693
danpatrickoneill <dan47573@gmail.com>
2019-10-02 18:56:23 -0400

    Added Player class w/ location and travel method

--- src/player.py
+++ src/player.py
@@ -1,2 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, location):
+        self.location = location
+
+    def getLocation(self):
+        return self.location
+
+    def setLocation(self, location):
+        self.location = location
+
+    def followPath(self, path):
+        try:
+            self.setLocation(self.location.paths[path])
+            print(f'Entering location: {self.location.name}')
+            return True
+        except:
+            print("There is no path in that direction. Please try again: ")
+            return False

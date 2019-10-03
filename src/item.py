c47dfc9cf8f9dd048c8160caa9a8d324932370c8
danpatrickoneill <dan47573@gmail.com>
2019-10-02 21:40:48 -0400

    Added base Item class

---
+++ src/item.py
@@ -0,0 +1,10 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def getName(self):
+        return self.name
+
+    def getDesc(self):
+        return self.description
